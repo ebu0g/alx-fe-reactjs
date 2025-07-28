554551252db286ffdbe6550cc511efba7b56d67c
Eb G <gebawak@gmail.com>
2025-07-28 15:38:25 +0300

    Initial commit

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', maxWidth: '600px', margin: 'auto' }}>
+      <h1>🍲 Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
