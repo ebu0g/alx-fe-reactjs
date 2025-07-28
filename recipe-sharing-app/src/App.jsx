5e83d05985e639ebac5e5f792fc140a1253b513f
Eb G <gebawak@gmail.com>
2025-07-28 16:44:38 +0300

    #    modified:   recipe-sharing-app/src/App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import SearchBar from './components/SearchBar';
+import { Routes, Route } from 'react-router-dom';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetail from './components/RecipeDetail';
 import RecipeList from './components/RecipeList';
 
 function App() {
   return (
-    <div style={{ maxWidth: '600px', margin: 'auto', padding: '16px' }}>
-      <h1>Recipe Sharing App</h1>
-      <SearchBar />
-      <RecipeList />
-    </div>
+    <Routes>
+      <Route path="/" element={<RecipeList />} />
+      <Route path="/add" element={<AddRecipeForm />} />
+      <Route path="/recipe/:id" element={<RecipeDetail />} />
+    </Routes>
   );
 }
 
