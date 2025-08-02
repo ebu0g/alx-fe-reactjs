7b5265a5c83b404bdbe8d80ff144128afeab60dd
Eb G <gebawak@gmail.com>
2025-08-02 09:13:01 +0300

    updated

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>GitHub User Search</h1>
+      {/* Future: SearchBar + Result Display */}
+    </div>
+  );
+}
+
+export default App;
