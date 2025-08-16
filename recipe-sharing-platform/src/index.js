78f9dcc971555b927b759e0772896cc19a1a7b9e
Eb G <gebawak@gmail.com>
2025-08-16 13:11:41 +0300

    .

--- recipe-sharing-platform/src/index.js
+++ recipe-sharing-platform/src/index.js
@@ -1,1 +1,11 @@
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';  // Tailwind CSS import
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
