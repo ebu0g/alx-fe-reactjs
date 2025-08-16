bf0b43ebc209de18cac354a6732d61eba7777295
Eb G <gebawak@gmail.com>
2025-08-16 12:17:39 +0300

    .

--- github-user-search/src/services/githubAPI.js
+++
@@ -1,7 +0,0 @@
-// src/services/githubService.js
-import axios from 'axios';
-
-export const fetchUserData = async (username) => {
-  const response = await axios.get(`https://api.github.com/users/${username}`);
-  return response.data;
-};
